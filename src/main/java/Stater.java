ba3276d89ad4a66f0b856c588e2ccd6ada9b4d71
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2025-09-26 09:28:45 +0530

    initial commit

---
+++ src/main/java/Stater.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Stater extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
